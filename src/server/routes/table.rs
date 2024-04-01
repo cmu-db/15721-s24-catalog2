4aaa8397742a46ec32b05762148f2baf8c2b19c8
Yen-Ju Wu <yenjuw@andrew.cmu.edu>
2024-04-01 16:35:27 -0400

    fix return type in table.rs

--- src/server/routes/table.rs
+++ src/server/routes/table.rs
@@ -5,8 +5,8 @@ use crate::response::*;
 use crate::request::*;
 use crate::server::routes::common::*;
 
-use crate::common::result::Result;
-
+use crate::common::result::{self, EmptyResult, ErrorType, JsonResult, Location, Result};
+use crate::err;
 // use rocket::serde::json::Json;
 // use rocket::Error;
 
@@ -28,6 +28,14 @@ pub fn get_table_by_namespace(namespace: &str) -> JsonResultGeneric<ListTablesRe
     },
   ];
 
+  let error = false;
+  if !error {
+    return err!(
+      ErrorType::NotFound,
+      Location::Table,
+      format!("Table not found")
+    );
+  }
   // Create and return ListTablesResponse
   let response = ListTablesResponse { identifiers };
 
@@ -87,98 +95,141 @@ pub fn get_table(namespace: &str, table: &str) -> JsonResultGeneric<LoadTableRes
 
 /// Commit updates to a table
 #[post("/namespaces/<namespace>/tables/<table>", data = "<commit_table_request>")]
-pub fn post_table(namespace: &str, table: &str, commit_table_request: Json<CommitTableRequest>) -> status::Custom<content::RawJson<&'static str>> {
-  let bad_request = namespace.is_empty() || table.is_empty();
-  // let bad_request = true;
-  let post_success = false;
-  let namespace_found = false;
-  let table_exist_already = false;
+pub fn post_table(namespace: &str, table: &str, commit_table_request: Json<CommitTableRequest>) -> JsonResultGeneric<CommitTableResponse> {
+  // let bad_request = namespace.is_empty() || table.is_empty();
+  // // let bad_request = true;
+  // let post_success = false;
+  // let namespace_found = false;
+  // let table_exist_already = false;
 
-  if bad_request{
-    status::Custom(Status::BadRequest, content::RawJson("{ \"Error 400 BadRequest\": \"Namespace or table name empty\" }")) 
-  } else {
-    if post_success{
-      status::Custom(Status::Ok, content::RawJson("")) // successful request 200
-    } else {
-      if !namespace_found{
-        status::Custom(Status::NotFound, content::RawJson("{ \"Error 404 NotFound\": \"Namespace not found\" }"))
-      } else {
-        if table_exist_already{
-          status::Custom(Status::Conflict, content::RawJson("{ \"Error 409 Conflict\": \"Table exist already\" }"))
-        } else {
-          status::Custom(Status::InternalServerError, content::RawJson("{ \"Error 5XX Others\": \"Server error\" }"))
-        }
-      }
-    }
-  }
+  // if bad_request{
+  //   status::Custom(Status::BadRequest, content::RawJson("{ \"Error 400 BadRequest\": \"Namespace or table name empty\" }")) 
+  // } else {
+  //   if post_success{
+  //     status::Custom(Status::Ok, content::RawJson("")) // successful request 200
+  //   } else {
+  //     if !namespace_found{
+  //       status::Custom(Status::NotFound, content::RawJson("{ \"Error 404 NotFound\": \"Namespace not found\" }"))
+  //     } else {
+  //       if table_exist_already{
+  //         status::Custom(Status::Conflict, content::RawJson("{ \"Error 409 Conflict\": \"Table exist already\" }"))
+  //       } else {
+  //         status::Custom(Status::InternalServerError, content::RawJson("{ \"Error 5XX Others\": \"Server error\" }"))
+  //       }
+  //     }
+  //   }
+  // }
+
+  // Generate metadata for the newly created table
+  let metadata = TableMetadata {
+    format_version: 1,
+    table_uuid: "generated_uuid".to_string(),
+    // Fill in other fields as needed
+  };
+
+  // Construct the response
+  let response = CommitTableResponse {
+    metadata,
+    metadata_location: "".to_string(),
+   };
+
+  // Return the response as JSON
+  Ok(Json(response))
 }
 
 /// Drop a table from the catalog
 #[delete("/namespaces/<namespace>/tables/<table>?<purge_requested..>")]
-pub fn delete_table(namespace: &str, table: &str, purge_requested: PurgeRequested) -> status::Custom<content::RawJson<&'static str>> {
-  // let bad_request = namespace.is_empty() || table.is_empty();
-  let bad_request = true;
-  let delete_success = true;
-  let table_found = false;
+pub fn delete_table(namespace: &str, table: &str, purge_requested: PurgeRequested) -> EmptyResult {
+  // // let bad_request = namespace.is_empty() || table.is_empty();
+  // let bad_request = true;
+  // let delete_success = true;
+  // let table_found = false;
 
-  if bad_request{
-    status::Custom(Status::BadRequest, content::RawJson("{ \"Error 400 BadRequest\": \"Namespace or table name empty\" }")) 
-  } else {
-    if delete_success{
-      status::Custom(Status::NoContent, content::RawJson("")) // 204 successful request
-    } else {
-      if !table_found{
-        status::Custom(Status::NotFound, content::RawJson("{ \"Error 404 NotFound\": \"Table not found\" }"))
-      } else {
-        status::Custom(Status::InternalServerError, content::RawJson("{ \"Internal server error\": \"testing\" }"))
-      }
-    }
+  // if bad_request{
+  //   status::Custom(Status::BadRequest, content::RawJson("{ \"Error 400 BadRequest\": \"Namespace or table name empty\" }")) 
+  // } else {
+  //   if delete_success{
+  //     status::Custom(Status::NoContent, content::RawJson("")) // 204 successful request
+  //   } else {
+  //     if !table_found{
+  //       status::Custom(Status::NotFound, content::RawJson("{ \"Error 404 NotFound\": \"Table not found\" }"))
+  //     } else {
+  //       status::Custom(Status::InternalServerError, content::RawJson("{ \"Internal server error\": \"testing\" }"))
+  //     }
+  //   }
+  // }
+  let error = false;
+  match !error {
+    true => Ok(()),
+    false => err!(
+      ErrorType::NotFound,
+      Location::Table,
+      format!("Table not found")
+    ),
   }
 }
 
 /// Check if a table exists
 #[head("/namespaces/<namespace>/tables/<table>")]
-pub fn head_table(namespace: &str, table: &str) -> status::Custom<content::RawJson<&'static str>> {
-  let bad_request = false;
-  let table_found = true;
-  let error_occur = true;
+pub fn head_table(namespace: &str, table: &str) -> EmptyResult {
+  // let bad_request = false;
+  // let table_found = true;
+  // let error_occur = true;
 
-  if bad_request{
-    status::Custom(Status::BadRequest, content::RawJson("{ \"Error 400 BadRequest\": \"Namespace or table name empty\" }")) 
-  } else {
-    if !error_occur{
-      if table_found{
-        status::Custom(Status::NoContent, content::RawJson("")) // 204 successful request
-      } else {
-        status::Custom(Status::NotFound, content::RawJson("{ \"Error 404 NotFound\": \"Table not found\" }"))
-      }
-    } else {
-      status::Custom(Status::InternalServerError, content::RawJson("{ \"Internal server error\": \"testing\" }"))
-    }
+  // if bad_request{
+  //   status::Custom(Status::BadRequest, content::RawJson("{ \"Error 400 BadRequest\": \"Namespace or table name empty\" }")) 
+  // } else {
+  //   if !error_occur{
+  //     if table_found{
+  //       status::Custom(Status::NoContent, content::RawJson("")) // 204 successful request
+  //     } else {
+  //       status::Custom(Status::NotFound, content::RawJson("{ \"Error 404 NotFound\": \"Table not found\" }"))
+  //     }
+  //   } else {
+  //     status::Custom(Status::InternalServerError, content::RawJson("{ \"Internal server error\": \"testing\" }"))
+  //   }
+  // }
+  let error = false;
+  match !error {
+    true => Ok(()),
+    false => err!(
+      ErrorType::NotFound,
+      Location::Table,
+      format!("Table not found")
+    ),
   }
 }
 
 /// Rename a table from its current name to a new name
 #[post("/tables/rename", data = "<rename_table_request>")]
-pub fn rename_table(rename_table_request: Json<RenameTableRequest>) -> status::Custom<content::RawJson<&'static str>> {
-  let bad_request = true;
-  let table_renamed = false;
-  let namespace_found = false;
-  let table_found = false;
+pub fn rename_table(rename_table_request: Json<RenameTableRequest>) -> EmptyResult {
+  // let bad_request = true;
+  // let table_renamed = false;
+  // let namespace_found = false;
+  // let table_found = false;
 
-  if bad_request{
-    status::Custom(Status::BadRequest, content::RawJson("{ \"Error 400 BadRequest\": \"Namespace or table name empty\" }"))
-  } else {
-    if table_renamed {
-      status::Custom(Status::NoContent, content::RawJson("")) // 204 successful request
-    } else {
-      if !namespace_found{
-        status::Custom(Status::NotFound, content::RawJson("{ \"Error 404 NotFound\": \"Namespace not found\" }"))
-      } else if !table_found{
-        status::Custom(Status::NotFound, content::RawJson("{ \"Error 404 NotFound\": \"Table not found\" }"))
-      } else {
-        status::Custom(Status::InternalServerError, content::RawJson("{ \"Internal server error\": \"testing\" }"))
-      }
-    }
+  // if bad_request{
+  //   status::Custom(Status::BadRequest, content::RawJson("{ \"Error 400 BadRequest\": \"Namespace or table name empty\" }"))
+  // } else {
+  //   if table_renamed {
+  //     status::Custom(Status::NoContent, content::RawJson("")) // 204 successful request
+  //   } else {
+  //     if !namespace_found{
+  //       status::Custom(Status::NotFound, content::RawJson("{ \"Error 404 NotFound\": \"Namespace not found\" }"))
+  //     } else if !table_found{
+  //       status::Custom(Status::NotFound, content::RawJson("{ \"Error 404 NotFound\": \"Table not found\" }"))
+  //     } else {
+  //       status::Custom(Status::InternalServerError, content::RawJson("{ \"Internal server error\": \"testing\" }"))
+  //     }
+  //   }
+  // }
+  let error = false;
+  match !error {
+    true => Ok(()),
+    false => err!(
+      ErrorType::NotFound,
+      Location::Table,
+      format!("Table not found")
+    ),
   }
 }
