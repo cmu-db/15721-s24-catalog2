127bb71722f699d7118a1ce36d6e02991b348c7b
zhouzilong <529620861@qq.com>
2024-04-02 14:13:56 -0400

    reorg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod arg;
 mod catalog;
 mod common;
 mod db;
@@ -7,48 +8,9 @@ mod util;
 #[macro_use]
 extern crate rocket;
 
-use common::result::EmptyResult;
 use db::DB;
 
-use server::routes::*;
-
-use crate::common::result::{ErrorType, Location};
-
-#[catch(404)]
-fn general_not_found() -> EmptyResult {
-  err!(
-    ErrorType::NotFound,
-    Location::Request,
-    "Resource not found, please check the URL".to_string()
-  )
-}
-
-#[catch(400)]
-fn general_bad_request() -> EmptyResult {
-  err!(
-    ErrorType::BadRequest,
-    Location::Request,
-    "Bad Request".to_string()
-  )
-}
-
-#[catch(422)]
-fn general_unprocessable_request() -> EmptyResult {
-  err!(
-    ErrorType::Unprocessable,
-    Location::Request,
-    "Unprocessable request".to_string()
-  )
-}
-
-#[catch(500)]
-fn general_internal_error() -> EmptyResult {
-  err!(
-    ErrorType::InternalError,
-    Location::Request,
-    "Internal server error".to_string()
-  )
-}
+use server::{catches, routes::*};
 
 #[launch]
 fn rocket() -> _ {
@@ -59,16 +21,8 @@ fn rocket() -> _ {
 
   rocket::build()
     .attach(namespace::stage())
+    .attach(catches::stage())
     .manage(db.unwrap())
-    .register(
-      "/",
-      catchers![
-        general_not_found,
-        general_bad_request,
-        general_internal_error,
-        general_unprocessable_request
-      ],
-    )
     .mount(
       "/v1",
       routes![
---
+++ src/server/catches.rs
@@ -0,0 +1,52 @@
+use crate::common::result::{EmptyResult, ErrorType, Location};
+use crate::err;
+
+#[catch(404)]
+fn general_not_found() -> EmptyResult {
+  err!(
+    ErrorType::NotFound,
+    Location::Request,
+    "Resource not found, please check the URL".to_string()
+  )
+}
+
+#[catch(400)]
+fn general_bad_request() -> EmptyResult {
+  err!(
+    ErrorType::BadRequest,
+    Location::Request,
+    "Bad Request".to_string()
+  )
+}
+
+#[catch(422)]
+fn general_unprocessable_request() -> EmptyResult {
+  err!(
+    ErrorType::Unprocessable,
+    Location::Request,
+    "Unprocessable request".to_string()
+  )
+}
+
+#[catch(500)]
+fn general_internal_error() -> EmptyResult {
+  err!(
+    ErrorType::InternalError,
+    Location::Request,
+    "Internal server error".to_string()
+  )
+}
+
+pub fn stage() -> rocket::fairing::AdHoc {
+  rocket::fairing::AdHoc::on_ignite("namespace", |rocket| async {
+    rocket.register(
+      "/",
+      catchers![
+        general_not_found,
+        general_bad_request,
+        general_internal_error,
+        general_unprocessable_request
+      ],
+    )
+  })
+}
