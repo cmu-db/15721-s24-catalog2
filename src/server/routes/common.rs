ae070d22d4e7fb41eaeec13f52d6087a52b3c55b
Yen-Ju Wu <yenjuw@andrew.cmu.edu>
2024-04-02 17:30:38 -0400

    add structs for Schema

--- src/server/routes/common.rs
+++ src/server/routes/common.rs
@@ -1,4 +1,4 @@
-use crate::catalog::namespace::Namespace;
+// use crate::catalog::namespace::Namespace;
 use rocket::serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -11,3 +11,70 @@ pub struct TableIdentifier {
 #[derive(Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
 pub struct NamespaceResponse(pub Vec<String>);
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+#[serde(untagged)]
+pub enum Type {
+  Primitive(PrimitiveType),
+  Struct(StructType),
+  List(Box<ListType>),
+  Map(Box<MapType>),
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct PrimitiveType(String);
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct StructField {
+  id: i32,
+  name: String,
+  #[serde(flatten)]
+  type_: Type,
+  required: bool,
+  // doc: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct StructType {
+  #[serde(rename = "type")]
+  type_: String,
+  fields: Vec<StructField>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ListType {
+  #[serde(rename = "type")]
+  type_: String,
+  element_id: i32,
+  #[serde(flatten)]
+  element: Type,
+  element_required: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct MapType {
+  #[serde(rename = "type")]
+  type_: String,
+  key_id: i32,
+  #[serde(flatten)]
+  key: Type,
+  value_id: i32,
+  #[serde(flatten)]
+  value: Type,
+  value_required: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Schema {
+  #[serde(flatten)]
+  struct_type: StructType,
+  schema_id: i32,
+  identifier_field_ids: Vec<i32>,
+}
